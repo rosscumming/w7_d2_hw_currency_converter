9e09a37c7717a4f2010cf7b3d781598f9435c30f
Ross <rosso5792@gmail.com>
2020-01-14 17:43:41 +0000

    managed to show keys in drop down

--- src/app.js
+++ src/app.js
@@ -4,10 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      currencies: null,
+      selectedCurrency: null
+    },
+    mounted(){
+      this.currencyFetch()
     },
     methods: {
-
+      currencyFetch: function () {
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => this.currencies = data.rates)
+        this.currencies = null;
+      }
     }
   });
 });
